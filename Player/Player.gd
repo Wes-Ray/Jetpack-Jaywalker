23fc390c97743b5ef2d7633354430d558a40dc3b
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2023-07-07 22:12:38 -0500

    basic jet pack implemented

--- Player/Player.gd
+++ Player/Player.gd
@@ -6,36 +6,48 @@ export var max_fall_speed := 300.0
 #export var friction_strength := 25.0  # not used right now, friction is based on walk_force
 
 export var walk_force := 25.0
-export var max_walk_speed := 200.0
+export var max_walk_speed := 300.0
 
 export var jump_strength := 200.0
 
 export var jet_pack_fuel_duration := 1.5
-export var jet_pack_thrust_strength := 100.0
+export var max_jetpack_speed := 200.0
+export var jet_pack_thrust_strength := 60.0
 
 var velocity := Vector2.ZERO
 
 enum State {IDLE, RUN, FALL, JUMP, JET_PACK, ON_WALL}
 var player_state = State.IDLE
+var is_jumping = false
 
 onready var debug_state_label := $debug_state_label
 onready var debug_velocity_label := $debug_velocity_label
+onready var debug_misc_label := $debug_misc_label
 
 
 func _ready() -> void:
 	pass
 
 
-func _physics_process(delta: float) -> void:
-	# get input
+func _physics_process(_delta: float) -> void:
+	###############################################################################################
+	# INPUT
+	###############################################################################################
+	
 	var walk_input = Input.get_axis("move_left", "move_right")
-	var is_jumping = Input.is_action_pressed("move_jump")
 	
+	# set player to is_jumping manuall between press and release, this allows game logic to turn
+	# off jumping if needed without inputs overriding game logic. 
+	if is_jumping and Input.is_action_just_released("move_jump"):
+		is_jumping = false
+	elif (not is_jumping) and Input.is_action_just_pressed("move_jump"):
+		is_jumping = true
 	
-	# determine state (may have to be done in the state code below)
+#	debug_misc_label.text = str(is_jumping)
 	
-	
-	# move player based on state
+	###############################################################################################
+	# STATE MACHINE
+	###############################################################################################
 	# TODO: consider adding floor detect distance
 	match player_state:
 		State.IDLE:
@@ -56,16 +68,23 @@ func _physics_process(delta: float) -> void:
 			debug_state_label.text = "FALL"
 			if is_on_floor():
 				player_state = State.IDLE
+			if is_jumping:
+				player_state = State.JET_PACK
 		
 		State.JUMP:
 			debug_state_label.text = "JUMP"
 			if not is_on_floor():
+				is_jumping = false
 				player_state = State.FALL
 			
 			velocity.y = -jump_strength
 		
 		State.JET_PACK:
 			debug_state_label.text = "JET_PACK"
+			if not is_jumping:
+				player_state = State.FALL
+			
+			velocity.y = move_toward(velocity.y, -max_jetpack_speed, jet_pack_thrust_strength)
 		
 		State.ON_WALL:
 			debug_state_label.text = "ON_WALL"
@@ -73,7 +92,12 @@ func _physics_process(delta: float) -> void:
 		_:
 			debug_state_label.text = "ERROR"
 	
+	###############################################################################################
+	# MOVEMENT
+	###############################################################################################
 	# note: consider move_and_slide_with_snap() if the player goes down ramps
+	# note: may need to move velocity updates into states themselves, but could also just adjust
+	# movement multipliers in each state as appropriate
 	velocity.x = move_toward(velocity.x, walk_input * max_walk_speed, walk_force)
 	velocity.y = move_toward(velocity.y, max_fall_speed, gravity_strength)
 	
