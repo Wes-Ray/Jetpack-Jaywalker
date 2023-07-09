f248fa4fb14d6338fcc0301fde96d1b67cd97170
Brian Guerrero <BGuerrero825@gmail.com>
2023-07-08 22:01:09 -0400

    fix

--- Zap/Zap.gd
+++ Zap/Zap.gd
@@ -7,8 +7,8 @@ extends Node2D
 
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	$Area2D.connect("area_entered", self, "_on_Area2D_area_entered")
+# func _ready():
+#	pass
 
 func _on_Area2D_area_entered(area: Area2D) -> void:
 	Orchestrator.apply_damage(area)
