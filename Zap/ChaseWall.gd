ac421fdf98c921b9d3644762033d0955291a556f
Brian Guerrero <BGuerrero825@gmail.com>
2023-11-18 22:51:10 -0600

    chasewall jitter fix, updated cannon art
    chasewall now moves on physics_process vs regular process, eliminating the jitter on camera scroll. Made a new cannon asset using rotated pixel art :O !!!! Also added lights to the projection laser and the projectile.

--- Zap/ChaseWall.gd
+++ Zap/ChaseWall.gd
@@ -8,5 +8,5 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta: float) -> void:
 	position.x += speed * delta
