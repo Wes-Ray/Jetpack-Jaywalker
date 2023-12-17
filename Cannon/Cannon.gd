f13cce194583b8b4bb084ffc5f5ee2bf3a29a36b
Brian Guerrero <BGuerrero825@gmail.com>
2023-12-17 14:20:37 -0600

    raycasted laser sprite and collision box

--- Cannon/Cannon.gd
+++ Cannon/Cannon.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+onready var laser = $Body/Laser
+onready var raycast = $Body/Laser/RayCast2D
+onready var collider = $Body/Laser/CollisionShape2D
+onready var sprite = $Body/Laser/Sprite
 
 
 # Called when the node enters the scene tree for the first time.
@@ -14,3 +14,21 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func project_beam():
+	raycast.enabled = true
+	raycast.force_raycast_update()
+	var laser_start_pos = laser.global_position
+	var collision_pos = raycast.get_collision_point()
+	var laser_length = laser_start_pos.distance_to(collision_pos)
+	var laser_midpoint = (laser_length/2)
+	print("laser pos: " , laser_start_pos, "   collision_pos: ", collision_pos)
+	print("LASER_LENGTH: " , laser_length, "    LASER_MIDPOINT: " , laser_midpoint)
+	collider.position.y = laser_midpoint
+	print(collider.position.y) 
+	sprite.position.y = laser_midpoint
+	collider.shape.height  = laser_length
+	print(collider.shape.height)
+	sprite.region_rect.size.y = laser_length
+
+
